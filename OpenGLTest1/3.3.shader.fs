f948caef5bc8e6cae0e6f6d61b84f56752e4d7bc
Jonas Allemann <jonas.allemann@ibm.com>
2019-03-24 17:57:48 +0100

    PC added container and awesomeface texture

--- OpenGLTest1/3.3.shader.fs
+++ OpenGLTest1/3.3.shader.fs
@@ -2,10 +2,15 @@
 // ================
 #version 330 core
 out vec4 FragColor;
-// in vec3 ourColor;
-in vec3 ourPosition;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = vec4(ourPosition, 1.0);    // note how the position value is linearly interpolated to get all the different colors
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
