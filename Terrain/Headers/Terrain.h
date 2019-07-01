6756cd6071ea17adaf7e9e7ba7b29d3338c3b663
Jonas Allemann <jonas.allemann@ibm.com>
2019-07-01 11:46:02 +0100

    terrain_opt changed stuff

--- Terrain/Headers/Terrain.h
+++ Terrain/Headers/Terrain.h
@@ -21,9 +21,12 @@ private:
 	PerlinNoise pNoise;
 
 	int getIndex(int i, int j) { return j * width + i; };
+	int getMeshIndex(int i, int j) { return j * (width -1) * 9 * 6 + i * 9 * 6; };
 
+	void setMeshColour();
+	void setMeshCoordinates();
 	void generateHeightMap(float &elevation, float &frequency, unsigned int &octaves, float &persistence);
-	void addTriangleToMesh(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3);
-	void addPointToMesh(glm::vec3 v);
+	void setHeightInMesh(int i, int j, float h1, float h2, float h3, float h4);
+	void addPointToMesh(int offset, glm::vec3 v);
 };
 
--- Terrain/Terrain.cpp
+++ Terrain/Terrain.cpp
@@ -10,10 +10,55 @@ Terrain::Terrain(int width, int height)
 	terrainColour = glm::vec3(0.3f, 0.15f, 0.05f);
 
 	heightMap.resize(width * height);
+	vertices.resize((width - 1) * (height - 1) * 9 * 6);
+
+	setMeshColour();
+	setMeshCoordinates();
 }
 
 Terrain::~Terrain()
 {
+}
+
+void Terrain::setMeshColour()
+{
+	for (int offset = 0; offset < vertices.size(); offset += 9) {
+		addPointToMesh(offset + 6, terrainColour);
+	}
+}
+
+void Terrain::setMeshCoordinates()
+{
+	for (int j = 0; j < height - 1; j++) {
+		for (int i = 0; i < width - 1; i++) {
+
+			glm::vec3 v1, v2, v3, v4;
+
+			v1.x = (float)i;
+			v1.y = (float)0;
+			v1.z = (float)j;
+
+			v2.x = (float)i + 1;
+			v2.y = (float)0;
+			v2.z = (float)j;
+
+			v3.x = (float)i + 1;
+			v3.y = (float)0;
+			v3.z = (float)j + 1;
+
+			v4.x = (float)i;
+			v4.y = (float)0;
+			v4.z = (float)j + 1;
+
+			addPointToMesh(getMeshIndex(i, j) + 9 * 0, v1);
+			addPointToMesh(getMeshIndex(i, j) + 9 * 1, v2);
+			addPointToMesh(getMeshIndex(i, j) + 9 * 2, v3);
+
+			addPointToMesh(getIndex(i, j) + 9 * 3, v1);
+			addPointToMesh(getIndex(i, j) + 9 * 4, v3);
+			addPointToMesh(getIndex(i, j) + 9 * 5, v4);
+		}
+	}
 }
 
 void Terrain::generateHeightMap(float &elevation, float &frequency, unsigned int &octaves, float &persistence)
@@ -33,58 +78,48 @@ void Terrain::generateHeightMap(float &elevation, float &frequency, unsigned int
 
 void Terrain::generateTerrain(float elevation, float frequency, unsigned int octaves, float persistence)
 {
-	vertices = {};
-	std::vector<glm::vec3> points = {};
-
 	generateHeightMap(elevation, frequency, octaves, persistence);
 
 	for (int j = 0; j < height - 1; j++) {
 		for (int i = 0; i < width - 1; i++) {
 
-			glm::vec3 v1, v2, v3, v4;
+			float h1, h2, h3, h4;
 
-			v1.x = (float)i;
-			v1.y = heightMap[getIndex(i, j)];
-			v1.z = (float)j;
+			h1 = heightMap[getIndex(i, j)];
+			h2 = heightMap[getIndex(i + 1, j)];
+			h3 = heightMap[getIndex(i + 1, j + 1)];
+			h4 = heightMap[getIndex(i, j + 1)];
 
-			v2.x = (float)i+1;
-			v2.y = heightMap[getIndex(i + 1, j)];
-			v2.z = (float)j;
-
-			v3.x = (float)i+1;
-			v3.y = heightMap[getIndex(i + 1, j + 1)];
-			v3.z = (float)j+1;
-
-			v4.x = (float)i;
-			v4.y = heightMap[getIndex(i, j + 1)];
-			v4.z = (float)j+1;
-
-			addTriangleToMesh(v1, v2, v3);
-			addTriangleToMesh(v1, v3, v4);
+			setHeightInMesh(i, j, h1, h2, h3, h4);
 		}
 	}
 }
 
-void Terrain::addPointToMesh(glm::vec3 v)
+void Terrain::addPointToMesh(int offset, glm::vec3 v)
 {
-	vertices.push_back(v.x);
-	vertices.push_back(v.y);
-	vertices.push_back(v.z);
+	vertices[offset] = v.x;
+	vertices[offset + 1] = v.y;
+	vertices[offset + 2] = v.z;
 }
 
-void  Terrain::addTriangleToMesh(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3)
+void  Terrain::setHeightInMesh(int i, int j, float h1, float h2, float h3, float h4)
 {
-	glm::vec3 n = glm::normalize(glm::cross(v1 - v2, v3 - v1));
+	glm::vec3 n1 = glm::normalize(glm::cross(glm::vec3(-1 , h1 - h2, 0), glm::vec3(1, h3 - h1, 1)));
+	glm::vec3 n2 = glm::normalize(glm::cross(glm::vec3(0, h4 - h1, -1), glm::vec3(-1, h3 - h4, 0)));
 	
-	addPointToMesh(v1);
-	addPointToMesh(n);
-	addPointToMesh(terrainColour);
+	vertices[getMeshIndex(i, j) + 9 * 0 + 1] = h1;
+	vertices[getMeshIndex(i, j) + 9 * 1 + 1] = h2;
+	vertices[getMeshIndex(i, j) + 9 * 2 + 1] = h3;
 
-	addPointToMesh(v2);
-	addPointToMesh(n);
-	addPointToMesh(terrainColour);
+	vertices[getMeshIndex(i, j) + 9 * 3 + 1] = h3;
+	vertices[getMeshIndex(i, j) + 9 * 4 + 1] = h4;
+	vertices[getMeshIndex(i, j) + 9 * 5 + 1] = h1;
 
-	addPointToMesh(v3);
-	addPointToMesh(n);
-	addPointToMesh(terrainColour);
+	addPointToMesh(getMeshIndex(i, j) + 9 * 0 + 3, n1);
+	addPointToMesh(getMeshIndex(i, j) + 9 * 1 + 3, n1);
+	addPointToMesh(getMeshIndex(i, j) + 9 * 2 + 3, n1);
+
+	addPointToMesh(getMeshIndex(i, j) + 9 * 3 + 3, n2);
+	addPointToMesh(getMeshIndex(i, j) + 9 * 4 + 3, n2);
+	addPointToMesh(getMeshIndex(i, j) + 9 * 5 + 3, n2);
 }
