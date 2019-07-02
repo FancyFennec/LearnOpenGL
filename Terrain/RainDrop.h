69aafd21433dc99f3a51ba8d24ac7b41a4e2a15d
Jonas Allemann <jonas.allemann@ibm.com>
2019-07-02 07:57:10 +0100

    erosion added soome getters and setters

--- Terrain/RainDrop.h
+++ Terrain/RainDrop.h
@@ -15,6 +15,22 @@ public:
 	float yPos;
 	std::vector<float> gradient = { 0, 0 };
 
+	float p_inertia = 0.1f;
+	float p_minSlope = 0.1f;
+	float p_capacity = 0.1f;
+	float p_deposition = 0.1f;
+	float p_erosion = 0.1f;
+	float p_evaporation = 0.1f;
+	float p_gravity = 9.81f;
+
+	void setInertia(float inertia) { p_inertia = inertia; };
+	void setMinSlope(float minSlope) { p_minSlope = minSlope; };
+	void setCapazity(float capacity) { p_capacity = capacity; };
+	void setDeposition(float deposition) { p_deposition = deposition; };
+	void setErosion(float erosion) { p_erosion = erosion; };
+	void setEvaporation(float evaporation) { p_evaporation = evaporation; };
+	void setGravity(float gravity) { p_gravity = gravity; };
+
 private:
 	int getIndex(int i, int j) { return j * width + i; };
 
